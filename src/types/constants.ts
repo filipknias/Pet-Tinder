31d7d2c02b47843775012339531ad7481a04b9ab
Filip <filip.knias@o2.pl>
2021-10-05 21:40:53 +0200

    Commit #34

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,4 +1,5 @@
-export const PROXY_SERVER = "https://thingproxy.freeboard.io/fetch";
+// export const PROXY_SERVER = "https://thingproxy.freeboard.io/fetch";
+export const PROXY_SERVER = "http://www.whateverorigin.org/get?url=";
 export const LOCAL_STORAGE_TOKEN_KEY = "PET_TINDER_TOKEN";
 export const LOCAL_STORAGE_FILTERS_KEY = "PET_TINDER_FILTERS";
 export const LOCAL_STORAGE_COOKIES_KEY = "PET_TINDER_COOKIES";
